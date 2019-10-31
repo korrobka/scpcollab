56448db4ba04c91879cacde10455ec8c90baeaeb
korrobka <49925319+korrobka@users.noreply.github.com>
2019-10-31 06:07:17 +0300

    massive update with all pics and stuff

---
+++ fill.js
@@ -0,0 +1,55 @@
+"use strict"
+
+window.txt = [];
+function parse(data) {
+	window.txt = JSON.parse(data);
+}
+
+parse(en)
+while(window.txt.length>0){
+addtile(window.txt.pop());
+}
+
+parse(cr)
+while(window.txt.length>0){
+addcredit(window.txt.pop());
+}
+// en = "@aneckdope~1123; 4443.";
+// addcredit(en);
+
+function addtile(entryname) {
+	let a = document.createElement('a');
+	let li = document.createElement('li');
+	let inhtml;
+
+	a.target = 	"_blank";
+	a.href = 	"http://www.scp-wiki.net/"+
+				entryname;
+
+	inhtml = 	"<div class=\"image\"><img src=\"img/scp/"+
+				entryname +
+				".gif\"></div>";
+	li.insertAdjacentHTML("beforeend",inhtml);
+	inhtml = 	"<div class=\"overlay image\"><img src=\"img/overlay.png\"></div>";
+	li.insertAdjacentHTML("beforeend",inhtml);
+	a.insertAdjacentElement("beforeend",li);
+	entries.prepend(a);
+}
+
+function addcredit(creditstring) {
+	let link
+	let tpos = creditstring.indexOf("~",0)
+	if (creditstring[0]=="@") link = "href=\"https://twitter.com/"+ creditstring.slice(1,tpos) +"\"";
+	else link = "";
+
+	let li = document.createElement('li');
+	let inhtml;
+
+	inhtml = 	"<a target=\"_blank\" " +
+				link + ">" +
+				creditstring.slice(0, tpos ) + "</a>" +//name
+				"<p>~</p> <p>" +
+				creditstring.slice(tpos+1) + "</p>"; //pieces
+	li.insertAdjacentHTML("beforeend",inhtml);
+	collective.prepend(li);
+}
